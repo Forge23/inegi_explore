6d8393525864285491a40fcb59fe7b0b6620b689
Forge <angelxone@outlook.com>
2024-03-11 19:46:44 -0600

    codigo a modificar
    codigo base para empezar a usar el data set

---
+++ main.py
@@ -0,0 +1,33 @@
+import utils.imports as imp
+import utils.visuals as visu
+import utils.processing as proc
+
+# cargar dataset diabetes desde folder data
+dataset = imp.read_diabetes_dataset("data/diabetes.tab.txt")
+dataset2 = imp.read_diabetes_dataset("data/conjunto_de_datos_natalidad_2022.csv")
+
+# Generar y guardar los histogramas para una exploración inicial
+visu.save_histogram(dataset, "AGE")
+visu.save_histograms(dataset)
+visu.save_correlation(dataset, "BMI", "S6")
+visu.save_all_correlations(dataset, dataset.corr())
+visu.save_all_correlations_one_image(dataset)
+
+norm_dataset = proc.normalize_diabetes_data(dataset)
+training_data, test_data = proc.split_data(norm_dataset, 0.7)
+training_input = training_data[['AGE', 'SEX', 'BMI', 'BP', 'S1', 'S2', 'S3', 'S4', 'S5', 'S6']]
+training_output = training_data["Y"]
+model = proc.simple_linear_regression(training_input, training_output, 10)
+
+test_input = training_data[['AGE', 'SEX', 'BMI', 'BP', 'S1', 'S2', 'S3', 'S4', 'S5', 'S6']]
+test_output = training_data["Y"]
+test_predictions = proc.test_predictions(model, test_input, 10)
+
+coefficients = proc.get_coefficients(model)
+print("Coefficients: ", coefficients)
+MSE = proc.get_mean_squared_error(test_output,
+                                  test_predictions)
+print("Mean Squared Error: ", MSE)
+R2 = proc.get_coefficient_determination(test_output,
+                                        test_predictions)
+print("R² Score: ", R2)
---
+++ utils/processing.py
@@ -0,0 +1,83 @@
+import os
+import math as mt
+import pandas as pd
+from sklearn import linear_model
+from sklearn.metrics import mean_squared_error, r2_score
+
+
+def check_output_folder(path):
+    if os.path.isdir(path):
+        pass
+    else:
+        os.makedirs(path)
+        print(path + " folder created")
+
+
+def save_correlations(data):
+    check_output_folder("output")
+    correlations = data.corr()
+    # Guardar las correlaciones en un archivo CSV
+    correlations.to_csv("output/correlations.csv")
+    print("Correlation data saved as 'output/correlations.csv'")
+
+def save_correlations2(data2):
+    check_output_folder("output")
+    correlations = data2.corr()
+    # Guardar las correlaciones en un archivo CSV
+    correlations.to_csv("output/correlations_inegi.csv")
+    print("Correlation data saved as 'output/correlations_inegi.csv'")    
+
+
+def get_correlations(data):
+    # Calcular las correlaciones
+    correlations = data.corr()
+
+    # Guardar las correlaciones en un archivo CSV
+    save_correlations(data)
+
+    # Devolver el dataframe de correlaciones
+    return correlations
+
+
+def normalize_diabetes_data(data):
+    # formula ->  Z = (1/sqrt(n)) * ((x-mu)/std)
+    mu_data = data.mean()
+    std_data = data.std()
+    normalized_data = data.sub(mu_data, axis='columns')
+    normalized_data = normalized_data.div(std_data, axis='columns')
+    val = (1 / mt.sqrt(data.shape[0]))
+    normalized_data = normalized_data.mul(val, axis='columns')
+    normalized_data["Y"] = data["Y"]
+    return normalized_data
+
+
+def split_data(data, split_percentage):
+    training_data = data.sample(frac=split_percentage)
+    idx_orig = data.index
+    idx_train = training_data.index
+    idx_test = idx_orig.drop(idx_train)
+    test_data = data.loc[idx_test]
+    return training_data, test_data
+
+
+def simple_linear_regression(input_train, output_train, cols):
+    regr = linear_model.LinearRegression()
+    inputs = input_train.values.reshape(-1, cols)
+    regr.fit(inputs, output_train)
+    return regr
+
+def test_predictions(model, input_test, columns):
+    output_test = model.predict(input_test.values.reshape(-1, columns))
+    return output_test
+
+
+def get_coefficients(model):
+    return model.coef_
+
+
+def get_mean_squared_error(output_test, output_predicted):
+    return mean_squared_error(output_test, output_predicted)
+
+
+def get_coefficient_determination(output_test, output_predicted):
+    return r2_score(output_test, output_predicted)
